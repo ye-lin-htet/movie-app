ef6e6e544363794bc95ced5433e4bc78569af652
antonious <yaelinhtet98@gmail.com>
2019-11-24 10:22:38 +0630

    project done

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import LatestMovie from '@/components/LatestMovies'
+import Movie from '@/components/Movie'
+import SearchMovie from '@/components/SearchMovie'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      name: 'LatestMovie',
+      component: LatestMovie
+    },
+    {
+      path: '/movie/:id',
+      name: 'Movie',
+      props: true,
+      component: Movie
+    },
+    {
+      path: '/search/:name',
+      name: 'SearchMovie',
+      props: true,
+      component: SearchMovie
+    }
+  ],
+  mode: 'history'
+})
